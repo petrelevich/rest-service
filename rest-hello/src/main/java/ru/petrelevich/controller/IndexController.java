9347566f5e813ca3e7de5b42e291bbedf4cc6c4a
sergey <petrelevich@yandex.ru>
2022-01-16 13:22:16 +0300

    extended controller

--- rest-hello/src/main/java/ru/petrelevich/controller/IndexController.java
+++ rest-hello/src/main/java/ru/petrelevich/controller/IndexController.java
@@ -1,6 +1,10 @@
 package ru.petrelevich.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -10,8 +14,13 @@ import java.net.UnknownHostException;
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() throws UnknownHostException {
-        return "Hi, it works, host: " + InetAddress.getLocalHost().getHostName();
+    @GetMapping("/hi")
+    public String hi(@RequestParam(name="name") String name) throws UnknownHostException {
+        return String.format("Hi, %s. It works, host: %s", name, InetAddress.getLocalHost().getHostName());
+    }
+
+    @PostMapping("/response/{name}")
+    public Response response(@PathVariable("name") String name, @RequestBody Request params) throws UnknownHostException {
+        return new Response(name, String.format("%s-%s", params.param1(), params.param2()));
     }
 }
