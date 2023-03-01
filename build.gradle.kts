47f51d3bad8e8ec7b4baf74f56bfea3a2813802d
sergey <petrelevich@yandex.ru>
2023-03-01 22:05:51 +0300

    migration to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,92 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.gradle.api.plugins.JavaPluginExtension
+import fr.brouillard.oss.gradle.plugins.JGitverPluginExtension
+import fr.brouillard.oss.gradle.plugins.JGitverPlugin
+
+plugins {
+    idea
+    id("fr.brouillard.oss.gradle.jgitver")
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+}
+
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(17)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.petrelevich"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+            }
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing", "-Werror"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    plugins.apply(JGitverPlugin::class.java)
+    extensions.configure<JGitverPluginExtension> {
+        strategy("PATTERN")
+        nonQualifierBranches("main,master")
+        tagVersionPattern("\${v}\${<meta.DIRTY_TEXT}")
+        versionPattern(
+            "\${v}\${<meta.COMMIT_DISTANCE}\${<meta.GIT_SHA1_8}" +
+                    "\${<meta.QUALIFIED_BRANCH_NAME}\${<meta.DIRTY_TEXT}-SNAPSHOT"
+        )
+    }
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                .managedVersions
+                .toSortedMap()
+                .map { "${it.key}:${it.value}" }
+                .forEach(::println)
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "rest-service"
+include ("rest-hello")
+
+pluginManagement {
+    val jgitver: String by settings
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+    val johnrengelmanShadow: String by settings
+    val jib: String by settings
+
+    plugins {
+        id("fr.brouillard.oss.gradle.jgitver") version jgitver
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+        id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+        id("com.google.cloud.tools.jib") version jib
+    }
+}
